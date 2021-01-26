95fa06ca50d19b827749794cf489cd388996f70f
Birajit <birajit95@gmail.com>
2021-01-27 01:50:54 +0530

    [Birajit] Refactor: Auth User Model is refactored

--- Auth/models.py
+++ Auth/models.py
@@ -3,5 +3,8 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     mobile = models.CharField(max_length=13, default=None, null=True)
-    role = models.CharField(choices=((1, 'Mentor'), (2, 'Engineer'), (3, 'Admin')), max_length=10)
+    role = models.CharField(choices=(('Mentor', 'Mentor'), ('Engineer', 'Engineer'), ('Admin', 'Admin')),
+                            max_length=10)
 
+    def get_full_name(self):
+        return f"{self.first_name} {self.last_name}"

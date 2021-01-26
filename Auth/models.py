53172b31ec8c017ff07250a136222b7bd211540a
Birajit <birajit95@gmail.com>
2021-01-27 01:07:04 +0530

    [Birajit] Add: User model is created

--- Auth/models.py
+++ Auth/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    mobile = models.CharField(max_length=13, default=None, null=True)
+    role = models.CharField(choices=((1, 'Mentor'), (2, 'Engineer'), (3, 'Admin')), max_length=10)
+

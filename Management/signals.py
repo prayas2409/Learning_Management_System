fedcafc4a4261e63277e3d4778a76133d24a28f8
Birajit <birajit95@gmail.com>
2021-01-27 22:26:18 +0530

    [Birajit] Refactor: Model signal is refactored

--- Management/signals.py
+++ Management/signals.py
@@ -1,6 +1,6 @@
 from django.dispatch import receiver
 from django.db.models.signals import post_save
-from .models import Student, Mentor
+from .models import Student, Mentor, Education
 import sys
 sys.path.append('..')
 from Auth.models import User
@@ -9,7 +9,8 @@ from Auth.models import User
 def create_student_or_mentor(sender, instance, created, **kwargs):
     if created:
         if instance.role == 'Engineer':
-            Student.objects.create(student=instance)
+            student = Student.objects.create(student=instance)
+            Education.objects.create(student=student)
         elif instance.role == 'Mentor':
             Mentor.objects.create(mentor=instance)
 

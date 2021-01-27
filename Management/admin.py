d66b922b46eecf2b7b317d37393097c585538fcd
Birajit <birajit95@gmail.com>
2021-01-27 22:23:09 +0530

    [Birajit] Add: StudentCourseMentor model is registered

--- Management/admin.py
+++ Management/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Course, Mentor, Student, Education, Performance
+from .models import Course, Mentor, Student, Education, Performance, StudentCourseMentor
 admin.site.register(Course)
 admin.site.register(Student)
 admin.site.register(Performance)
 admin.site.register(Mentor)
 admin.site.register(Education)
+admin.site.register(StudentCourseMentor)

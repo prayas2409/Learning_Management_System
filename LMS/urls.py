50a42eaa9a13a9fa93d7d8fb57e3afefcaf50a27
Birajit <birajit95@gmail.com>
2021-01-27 23:12:17 +0530

    [Birajit] Fix: A bug is fixed

--- LMS/urls.py
+++ LMS/urls.py
@@ -7,7 +7,7 @@ from drf_yasg import openapi
 
 schema_view = get_schema_view(
    openapi.Info(
-      title="FUNTOONOTES API DOCUMENTATION",
+      title="Learning Management System API DOCUMENTATION",
       default_version='v1',
       description="Test description",
       terms_of_service="https://www.google.com/policies/terms/",

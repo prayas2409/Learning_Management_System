04c8aa61cad25180cad6eeac973d0c8bc55df314
Birajit <birajit95@gmail.com>
2021-01-28 11:28:02 +0530

    [Birajit] Add: Email sending feature is added in UserRegistration API

--- Auth/views.py
+++ Auth/views.py
@@ -3,6 +3,10 @@ from rest_framework import status
 from rest_framework.generics import GenericAPIView
 from .serializers import UserSerializer
 from .permissions import isAdmin
+import sys
+sys.path.append('..')
+from LMS.mailConfirmation import Email
+from django.contrib.sites.shortcuts import get_current_site
 
 
 class UserRegistrationView(GenericAPIView):
@@ -10,7 +14,20 @@ class UserRegistrationView(GenericAPIView):
     permission_classes = (isAdmin,)
 
     def post(self, request):
+        """This API is used to Add user like Mentor,Engineer or another Admin to the system by an Admin
+         and informs the user about their account creation via email
+         request parms : user related data like username, name, email etc
+        """
         serializer = self.serializer_class(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
-        return Response({'response': f"user is added"}, status=status.HTTP_201_CREATED)
+        data = {
+            'name': f"{request.data['first_name']} {request.data['last_name']}",
+            'username': request.data['username'],
+            'password': request.data['password'],
+            'role': request.data['role'],
+            'email': request.data['email'],
+            'site': get_current_site(request).domain
+        }
+        Email.sendEmail(Email.configureAddUserEmail(data))
+        return Response({'response': f"A new {request.data['role']} is added"}, status=status.HTTP_201_CREATED)

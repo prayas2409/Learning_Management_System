4098367b2119a836bb9b00ebd7de397fc5016f0d
Birajit <birajit95@gmail.com>
2021-01-28 11:57:07 +0530

    [Birajit] Refactor: Admin permission response is refactored

--- Auth/permissions.py
+++ Auth/permissions.py
@@ -2,6 +2,8 @@ from rest_framework import permissions
 
 
 class isAdmin(permissions.BasePermission):
+    message = {'response': 'You are not an admin. Access Denied!'}
+
     def has_permission(self, request, view):
         if request.user.role == 'Admin':
             return True

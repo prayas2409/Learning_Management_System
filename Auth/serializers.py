65c6e1a901983ad29abcf4cf9b115f103c56a214
Birajit <birajit95@gmail.com>
2021-01-28 12:24:14 +0530

    [Birajit] Add: Login API is designed

--- Auth/serializers.py
+++ Auth/serializers.py
@@ -6,9 +6,10 @@ from django.contrib.auth.hashers import make_password
 class UserSerializer(serializers.ModelSerializer):
     confirm_password = serializers.CharField(min_length=6, max_length=15)
     email = serializers.EmailField(required=True)
+
     class Meta:
         model = User
-        fields = ['username', 'first_name', 'last_name', 'email', 'mobile', 'role', 'password', 'confirm_password' ]
+        fields = ['username', 'first_name', 'last_name', 'email', 'mobile', 'role', 'password', 'confirm_password']
 
     def validate(self, data):
         if User.objects.filter(email=data['email']).exists():
@@ -23,3 +24,7 @@ class UserSerializer(serializers.ModelSerializer):
         user = User.objects.create(**validated_data)
         return user
 
+
+class UserLoginSerializer(serializers.Serializer):
+    username = serializers.CharField(max_length=10, min_length=3, required=True)
+    password = serializers.CharField(max_length=20, min_length=6, required=True)
--- Auth/views.py
+++ Auth/views.py
@@ -1,13 +1,14 @@
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.generics import GenericAPIView
-from .serializers import UserSerializer
+from .serializers import UserSerializer, UserLoginSerializer
 from .permissions import isAdmin
 import sys
 sys.path.append('..')
 from LMS.mailConfirmation import Email
 from django.contrib.sites.shortcuts import get_current_site
-
+from django.contrib.auth import authenticate, login, logout
+from .JWTAuthentication import JWTAuth
 
 class UserRegistrationView(GenericAPIView):
     serializer_class = UserSerializer
@@ -31,3 +32,20 @@ class UserRegistrationView(GenericAPIView):
         }
         Email.sendEmail(Email.configureAddUserEmail(data))
         return Response({'response': f"A new {request.data['role']} is added"}, status=status.HTTP_201_CREATED)
+
+
+class UserLoginView(GenericAPIView):
+    serializer_class = UserLoginSerializer
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        username = serializer.data.get('username')
+        password = serializer.data.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user:
+            login(request, user)
+            request.session['username'] = JWTAuth.getToken(username=username, password=password)
+            return Response({'response': 'You are logged in'}, status=status.HTTP_200_OK)
+        return Response({'response': 'Bad credential found'}, status=status.HTTP_401_UNAUTHORIZED)
+

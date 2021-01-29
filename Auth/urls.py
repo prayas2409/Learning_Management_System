7cee8e198217d63fa5893dc12947ad8e1f26d2b0
Birajit <birajit95@gmail.com>
2021-01-30 02:15:47 +0530

    [Birajit] Add: forgot password and reset APIs urls are configured

--- Auth/urls.py
+++ Auth/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('login/', views.UserLoginView.as_view(), name='login'),
     path('logout/', views.UserLogoutView.as_view(), name='logout'),
     path('change-password/', views.ChangeUserPasswordView.as_view(), name='change-password'),
-
+    path('forgot-password/', views.ForgotPasswordView.as_view(), name='forgot-password'),
+    path('reset-password/<str:token>/', views.ResetPasswordView.as_view(), name='reset-password')
 ]
 

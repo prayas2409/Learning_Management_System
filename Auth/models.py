27f924d4647b4bd13e77c2518d5d022d6874f700
Birajit <birajit95@gmail.com>
2021-01-30 22:28:15 +0530

    [Birajit] Add: new field is added in user model

--- Auth/models.py
+++ Auth/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+
 class User(AbstractUser):
     mobile = models.CharField(max_length=13, unique=True)
     role = models.CharField(choices=(('Mentor', 'Mentor'), ('Engineer', 'Engineer'), ('Admin', 'Admin')),
                             max_length=10)
+    is_first_time_login = models.BooleanField(default=True)
 
     def get_full_name(self):
         return f"{self.first_name} {self.last_name}"

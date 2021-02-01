40f975500fcc69a3e1caf55d3e41ca2d8a9ec082
Birajit <birajit95@gmail.com>
2021-02-01 12:42:13 +0530

    [Birajit] Add: logger configuration is done

---
+++ LMS/loggerConfig.py
@@ -0,0 +1,8 @@
+import logging
+import os
+
+file_name = "Logs/LMS.log"
+os.makedirs(os.path.dirname(file_name), exist_ok=True)
+
+logging.basicConfig(filename=file_name, level=logging.INFO, format="%(asctime)s:%(levelname)s:%(message)s")
+log = logging.getLogger()

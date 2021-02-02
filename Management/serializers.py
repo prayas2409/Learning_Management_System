2581ac22e8626bbe9bea4a7606c5e93decbf2a97
Birajit <birajit95@gmail.com>
2021-02-03 02:07:48 +0530

    [Birajit] Refactor: Course Serializer is refactored

--- Management/serializers.py
+++ Management/serializers.py
@@ -5,7 +5,8 @@ from .models import Course
 class CourseSerializer(serializers.ModelSerializer):
     class Meta:
         model = Course
-        fields = ['course_name']
+        fields = ['id', 'course_name']
+        extra_kwargs = {'id': {'read_only': True}}
 
     def validate(self, data):
         data['course_name'] = data['course_name'].upper()

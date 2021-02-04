74c65c5e9339d65c9e2acf60dc380f5cbf09f343
Birajit <birajit95@gmail.com>
2021-02-05 00:24:43 +0530

    [Birajit] Add: New Permission is added

--- Auth/permissions.py
+++ Auth/permissions.py
@@ -8,3 +8,10 @@ class isAdmin(permissions.BasePermission):
         if request.user.role == 'Admin':
             return True
         return False
+
+
+class isMentorOrAdmin(permissions.BasePermission):
+    message = {'response': 'You are not an Admin or a Mentor. Access Denied!'}
+
+    def has_permission(self, request, view):
+        return request.user.role == 'Mentor' or request.user.role == 'Admin'

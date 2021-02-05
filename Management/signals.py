17326924b42674dd65cddb454660116ff8bc7930
Birajit <birajit95@gmail.com>
2021-02-05 20:47:49 +0530

    [Birajit] Fix: Hard code fixed

--- Management/signals.py
+++ Management/signals.py
@@ -4,14 +4,15 @@ from .models import Student, Mentor, Education
 import sys
 sys.path.append('..')
 from Auth.models import User
+from Auth.permissions import Role
 
 @receiver(signal=post_save, sender=User)
 def create_student_or_mentor(sender, instance, created, **kwargs):
     if created:
-        if instance.role == 'Engineer':
+        if instance.role == Role.STUDENT.value:
             student = Student.objects.create(student=instance)
             Education.objects.create(student=student)
-        elif instance.role == 'Mentor':
+        elif instance.role == Role.MENTOR.value:
             Mentor.objects.create(mentor=instance)
 
 

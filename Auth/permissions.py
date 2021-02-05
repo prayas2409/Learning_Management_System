f9d08b503d7e3461b50ee5cafb1990dd5ffcb2c8
Birajit <birajit95@gmail.com>
2021-02-05 15:14:00 +0530

    [Birajit] Add: new permission is added and other permission class are refactored

--- Auth/permissions.py
+++ Auth/permissions.py
@@ -1,11 +1,19 @@
 from rest_framework import permissions
+import enum
+
+
+class Role(enum.Enum):
+
+    ADMIN = 'Admin'
+    MENTOR = 'Mentor'
+    STUDENT = 'Engineer'
 
 
 class isAdmin(permissions.BasePermission):
     message = {'response': 'You are not an admin. Access Denied!'}
 
     def has_permission(self, request, view):
-        if request.user.role == 'Admin':
+        if request.user.role == Role.ADMIN.value:
             return True
         return False
 
@@ -14,4 +22,12 @@ class isMentorOrAdmin(permissions.BasePermission):
     message = {'response': 'You are not an Admin or a Mentor. Access Denied!'}
 
     def has_permission(self, request, view):
-        return request.user.role == 'Mentor' or request.user.role == 'Admin'
+        return request.user.role == Role.MENTOR.value or request.user.role == Role.STUDENT.value
+
+
+class OnlyStudent(permissions.BasePermission):
+    message = {'response': 'Only student can access'}
+
+    def has_permission(self, request, view):
+        print(Role.STUDENT)
+        return request.user.role == Role.STUDENT.value

01aefd48369ddc5dc752e72a660d48ac11a7402c
Birajit <birajit95@gmail.com>
2021-02-06 13:58:19 +0530

    [Birajit] Refactor: Multiple education fields are created for different degree using signal

--- Management/signals.py
+++ Management/signals.py
@@ -1,10 +1,12 @@
 from django.dispatch import receiver
 from django.db.models.signals import post_save
 from .models import Student, Mentor, Education, StudentCourseMentor, Performance
+from .utils import Degree
 import sys
 sys.path.append('..')
 from Auth.models import User
 from Auth.permissions import Role
+from .utils import Degree
 
 
 @receiver(signal=post_save, sender=User)
@@ -12,7 +14,8 @@ def create_student_or_mentor(sender, instance, created, **kwargs):
     if created:
         if instance.role == Role.STUDENT.value:
             student = Student.objects.create(student=instance)
-            Education.objects.create(student=student)
+            for degree in Degree:
+                Education.objects.create(student=student, degree=degree.value)
         elif instance.role == Role.MENTOR.value:
             Mentor.objects.create(mentor=instance)
 
@@ -23,4 +26,3 @@ def create_performace_record(sender, instance, created, **kwargs):
         for week_no in range(1, instance.course.duration_weeks+1):
             Performance.objects.create(student=instance.student, mentor=instance.mentor,
                                        course=instance.course, week_no=week_no)
-

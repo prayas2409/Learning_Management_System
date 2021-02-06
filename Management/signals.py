59b5cf3a974107911b9537dd241afa5a1fb9f097
Birajit <birajit95@gmail.com>
2021-02-06 10:42:10 +0530

    [Birajit] Add: Signal is build for auto creation of student performace instance during course assign

--- Management/signals.py
+++ Management/signals.py
@@ -1,10 +1,11 @@
 from django.dispatch import receiver
 from django.db.models.signals import post_save
-from .models import Student, Mentor, Education
+from .models import Student, Mentor, Education, StudentCourseMentor, Performance
 import sys
 sys.path.append('..')
 from Auth.models import User
 from Auth.permissions import Role
+
 
 @receiver(signal=post_save, sender=User)
 def create_student_or_mentor(sender, instance, created, **kwargs):
@@ -16,4 +17,10 @@ def create_student_or_mentor(sender, instance, created, **kwargs):
             Mentor.objects.create(mentor=instance)
 
 
+@receiver(signal=post_save, sender=StudentCourseMentor)
+def create_performace_record(sender, instance, created, **kwargs):
+    if created:
+        for week_no in range(1, instance.course.duration_weeks+1):
+            Performance.objects.create(student=instance.student, mentor=instance.mentor,
+                                       course=instance.course, week_no=week_no)
 

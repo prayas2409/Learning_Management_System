d68961c1952f5703f014c6eb3abc4163da5ff3ff
Birajit <birajit95@gmail.com>
2021-02-07 12:38:22 +0530

    [Birajit] Add: review results mail is configured

--- LMS/mailConfirmation.py
+++ LMS/mailConfirmation.py
@@ -26,6 +26,18 @@ class Email:
         return email_data
 
     @staticmethod
+    def configure_result_notification_mail(data):
+        email_body = f"Hello {data['name']} Greetings! \n" \
+                     f"Your week {data['week_no']} review results is following:\n" \
+                     f"Course: {data['course']} \n" \
+                     f"Mentor: {data['mentor']} \n" \
+                     f"Current Score:{data['score']} \n" \
+                     f"Remarks: {data['remark']} "
+
+        email_data = {'email_body': email_body, 'email_subject': 'Fellowship Review Results', 'to_email': data['email']}
+        return email_data
+
+    @staticmethod
     def sendEmail(data):
         email = EmailMessage(
             subject=data['email_subject'],

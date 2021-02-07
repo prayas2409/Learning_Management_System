93610e4095008e7e5b8e1920a886a014f208e347
Birajit <birajit95@gmail.com>
2021-02-07 22:58:53 +0530

    [Birajit] Add: Proper validation and doc string added in user serializer

--- Auth/serializers.py
+++ Auth/serializers.py
@@ -1,20 +1,30 @@
 from rest_framework import serializers
 from .models import User
-from django.contrib.auth.hashers import make_password
+from .permissions import Role
+import sys
+sys.path.append("..")
+from Management.utils import Pattern
 
 
 class UserSerializer(serializers.ModelSerializer):
+    """This Serializer is used to serialize user registration data
+    """
     email = serializers.EmailField(required=True)
+    first_name = serializers.RegexField(Pattern.NAME_PATTERN.value)
+    last_name = serializers.RegexField(Pattern.NAME_PATTERN.value)
+    mobile = serializers.RegexField(Pattern.MOBILE_PATTERN.value)
+    role = serializers.ChoiceField([Role.ADMIN.value, Role.STUDENT.value, Role.MENTOR.value])
 
     class Meta:
         model = User
         fields = ['username', 'first_name', 'last_name', 'email', 'mobile', 'role']
 
     def validate(self, data):
+        """If the mail id is already present in database then raising en exception
+        """
         if User.objects.filter(email=data['email']).exists():
             raise serializers.ValidationError("This email is already registered! Try with different one")
         return data
-
 
 
 class UserLoginSerializer(serializers.Serializer):
@@ -36,6 +46,7 @@ class ForgotPasswordSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['email']
+
 
 class ResetPasswordSerializer(serializers.Serializer):
     new_password = serializers.CharField(min_length=6, max_length=16, required=True)

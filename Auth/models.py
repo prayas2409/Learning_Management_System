11d9cea9a20e8cd19d4f45245906be2b9b35ca6c
Birajit <birajit95@gmail.com>
2021-02-08 11:13:11 +0530

    [Birajit] Add: Docstring is added in Auth APP models

--- Auth/models.py
+++ Auth/models.py
@@ -4,6 +4,9 @@ from django.utils.timezone import now
 
 
 class User(AbstractUser):
+    """This is the base user model which is build extending the AbstractUser models functionalities.
+    All the bassic fields like username, first_name, last_name, email, password etc are extended in this model"""
+
     mobile = models.CharField(max_length=13, unique=True)
     role = models.CharField(choices=(('Mentor', 'Mentor'), ('Engineer', 'Engineer'), ('Admin', 'Admin')),
                             max_length=10)
@@ -17,5 +20,7 @@ class User(AbstractUser):
 
 
 class TokenBlackList(models.Model):
+    """This model is used to store the used token so that we could check is the token is already used or not"""
+    
     token = models.CharField(max_length=300)
     time = models.DateTimeField(default=now)

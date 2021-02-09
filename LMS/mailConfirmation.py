92e4b2cd29a68b1c52fe46d9f0e51515f9f7b681
Birajit <birajit95@gmail.com>
2021-02-09 09:10:34 +0530

    [Birajit] Add: html templates are configured for email content fromat

--- LMS/mailConfirmation.py
+++ LMS/mailConfirmation.py
@@ -1,17 +1,21 @@
 from django.core.mail import EmailMessage
 import pyshorteners
 from django.urls import reverse
-
+from django.template.loader import render_to_string
 
 class Email:
     @staticmethod
     def configureAddUserEmail(data):
         absoluteURL = "http://" + data['site'] + reverse('login') + '?token=' + data['token']
-        email_body = f"Hi {data['name']}! You are added as a new {data['role']} " \
-                     f"\n Please login with the following credential \n" \
-                     f" Username: {data['username']}, password: {data['password']} \n" \
-                     f"Link: {absoluteURL}"
-        email_data = {'email_body': email_body, 'email_subject': 'Access Your Account', 'to_email': data['email']}
+        email_body_template = render_to_string('registration_email_template.html', {
+            'absolute_url': absoluteURL,
+            'name': data['name'],
+            'username': data['username'],
+            'password': data['password'],
+            'role': data['role']
+
+        })
+        email_data = {'email_body': email_body_template, 'email_subject': 'Access Your Account', 'to_email': data['email']}
         return email_data
 
     @staticmethod
@@ -19,21 +23,23 @@ class Email:
         absoluteURL = "http://" + data['site'] + reverse('reset-password', args=[data['token']])
         shortener = pyshorteners.Shortener()
         short_url = shortener.tinyurl.short(absoluteURL)
-        email_body = f"Hi {data['user'].first_name} {data['user'].last_name}! Reset your password at {data['site']}" \
-                     f" with the following link \n" \
-                     f"Link: {short_url}"
-        email_data = {'email_body': email_body, 'email_subject': 'Reset-Password', 'to_email': data['user'].email}
+        email_body = render_to_string('password_reset_mail_template.html', {
+            'name': data['name'],
+            'link': short_url
+        })
+        email_data = {'email_body': email_body, 'email_subject': 'Reset Your Password', 'to_email': data['email']}
         return email_data
 
     @staticmethod
     def configure_result_notification_mail(data):
-        email_body = f"Hello {data['name']} Greetings! \n" \
-                     f"Your week {data['week_no']} review results is following:\n" \
-                     f"Course: {data['course']} \n" \
-                     f"Mentor: {data['mentor']} \n" \
-                     f"Current Score:{data['score']} \n" \
-                     f"Remarks: {data['remark']} "
-
+        email_body = render_to_string('review_mail_template.html', {
+            'name': data['name'],
+            'marks': data['score'],
+            'week_no': data['week_no'],
+            'course': data['course'],
+            'mentor': data['mentor'],
+            'remarks': data['remark']
+        })
         email_data = {'email_body': email_body, 'email_subject': 'Fellowship Review Results', 'to_email': data['email']}
         return email_data
 
@@ -41,7 +47,7 @@ class Email:
     def sendEmail(data):
         email = EmailMessage(
             subject=data['email_subject'],
-            body=data['email_body'],
             to=(data['to_email'],)
         )
+        email.attach(content=data['email_body'], mimetype='text/html')
         email.send()

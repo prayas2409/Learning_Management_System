33a3348c5e9025447ec3309eadfc6017bb6bb065
malibharti5@gmail.com <malibharti5@gmail.com>
2021-02-12 04:20:36 +0530

    Bharti-ADD : Test cases for AddStudentAPI are refactored

--- Management/tests/test_views.py
+++ Management/tests/test_views.py
@@ -73,6 +73,14 @@ class ManagementAPITest(TestCase):
 
 ### Test cases for AddStudent API :
 
+    def login_method(self, credentials):
+        login = self.client.post(reverse('login'), data=json.dumps(credentials), content_type=CONTENT_TYPE)
+        token = login['authorization']
+        auth_headers = {
+            'HTTP_AUTHORIZATION': token,
+        }
+        return auth_headers
+
     def test_add_student_with_valid_payload_without_login(self):
         response = self.client.post(reverse('add-student'), data=json.dumps(self.valid_add_student_payload), content_type=CONTENT_TYPE)
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
@@ -83,39 +91,23 @@ class ManagementAPITest(TestCase):
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
     def test_add_student_with_valid_payload_after_login_by_admin_credentials(self):
-        login = self.client.post(reverse('login'), data=json.dumps(self.admin_login_payload), content_type=CONTENT_TYPE)
-        token = login['authorization']
-        auth_headers = {
-            'HTTP_AUTHORIZATION': token,
-        }
+        auth_headers = self.login_method(self.admin_login_payload)
         response = self.client.post(reverse('add-student'), **auth_headers, data=json.dumps(self.valid_add_student_payload), content_type=CONTENT_TYPE)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
     def test_add_student_with_invalid_payload_after_login_by_admin_credentials(self):
-        login = self.client.post(reverse('login'), data=json.dumps(self.admin_login_payload), content_type=CONTENT_TYPE)
-        token = login['authorization']
-        auth_headers = {
-            'HTTP_AUTHORIZATION': token,
-        }
+        auth_headers = self.login_method(self.admin_login_payload)
         response = self.client.post(reverse('add-student'), **auth_headers, data=json.dumps(self.invalid_add_student_payload), content_type=CONTENT_TYPE)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
 
     def test_add_student_with_valid_payload_after_login_by_mentor_credentials(self):
-        login = self.client.post(reverse('login'), data=json.dumps(self.mentor_login_payload), content_type=CONTENT_TYPE)
-        token = login['authorization']
-        auth_headers = {
-            'HTTP_AUTHORIZATION': token,
-        }
+        auth_headers = self.login_method(self.mentor_login_payload)
         response = self.client.post(reverse('add-student'), **auth_headers, data=json.dumps(self.valid_add_student_payload), content_type=CONTENT_TYPE)
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
 
     def test_add_student_with_valid_payload_after_login_by_student_credentials(self):
-        login = self.client.post(reverse('login'), data=json.dumps(self.student_login_payload), content_type=CONTENT_TYPE)
-        token = login['authorization']
-        auth_headers = {
-            'HTTP_AUTHORIZATION': token,
-        }
+        auth_headers = self.login_method(self.student_login_payload)
         response = self.client.post(reverse('add-student'), **auth_headers, data=json.dumps(self.valid_add_student_payload), content_type=CONTENT_TYPE)
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)

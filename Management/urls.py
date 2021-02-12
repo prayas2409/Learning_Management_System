52ea9f52c89e9fdf0a11840d0ae3030cc2c39917
Ranjith P <Ranjithroy704@gmail.com>
2021-02-12 15:36:09 +0530

    [Ranjith]Refactored: course api's url name is refactored

--- Management/urls.py
+++ Management/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('add-course/', views.AddCourseAPIView.as_view(), name='add-course'),
-    path('all-courses/', views.AllCoursesAPIView.as_view(), name='all-courses'),
+    path('course/', views.AddCourseAPIView.as_view(), name='add-course'),
+    path('courses/', views.AllCoursesAPIView.as_view(), name='all-courses'),
     path('update-course/<int:course_id>/', views.UpdateCourseAPIView.as_view(), name='update-course'),
     path('delete-course/<int:id>/', views.DeleteCourseAPIView.as_view(), name='delete-course'),
     path('course-mentor/<int:mentor_id>/', views.CourseToMentorMapAPIView.as_view(), name='course-mentor'),

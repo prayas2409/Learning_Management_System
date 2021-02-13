93a8bc44e3cb19bec7f00dd6cb2c91495b0dfe18
Birajit <birajit95@gmail.com>
2021-02-13 11:02:06 +0530

    [Birajit] Fix: Bug fixed

--- Auth/middlewares.py
+++ Auth/middlewares.py
@@ -14,13 +14,15 @@ class TokenAuthentication(object):
     def __call__(self, request, *args, **kwargs):
         token = request.headers.get('Authorization')
         jwtData = JWTAuth.verifyToken(token)
-        # if request.resolver_match.url_name == 'logout':
-        #     return self.get_response(request, *args, **kwargs)
         cache = Cache.getCacheInstance()
-        cache_token = cache.hget(jwtData.get('username'), 'auth')
+        cache_token = None
+        try:
+            cache_token = cache.hget(jwtData.get('username'), 'auth')
+        except Exception:
+            pass
         if cache_token:
             cache_token = cache_token.decode('utf-8')
-        if jwtData and cache_token:
+        if jwtData and cache_token == token:
             user = User.objects.get(username=jwtData.get('username'))
             request.META['user'] = user
             if not user.last_login == None or user.is_superuser:

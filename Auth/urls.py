b4a686cda70e9222600c822adb2ed3260459370e
Archana-Bhamare <archusonar2@gmail.com>
2021-02-16 20:14:42 +0530

    Archana-Refactor: Refactored the urls

--- Auth/urls.py
+++ Auth/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('add-user/', views.UserRegistrationView.as_view(), name='add-user'),
+    path('register-user/', views.UserRegistrationView.as_view(), name='register-user'),
     path('login/', views.UserLoginView.as_view(), name='login'),
     path('logout/', views.UserLogoutView.as_view(), name='logout'),
     path('change-password/', views.ChangeUserPasswordView.as_view(), name='change-password'),

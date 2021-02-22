20d177a44b6b577b682296bfc30500b08f6075fe
prayas2409 <prayas.hemraj.pagade@gmail.com>
2021-02-22 12:13:57 +0530

    [Prayas] Refactored: Change password is not manadatory for first time login

--- Auth/middlewares.py
+++ Auth/middlewares.py
@@ -13,37 +13,22 @@ class TokenAuthentication(object):
 
     def __call__(self, request, *args, **kwargs):
         token = request.headers.get('Authorization')
-        jwtData = JWTAuth.verifyToken(token)
-        cache = Cache.getCacheInstance()
-        cache_token = None
-        try:
-            cache_token = cache.hget(jwtData.get('username'), 'auth')
-        except Exception:
-            pass
-        if cache_token:
-            cache_token = cache_token.decode('utf-8')
-        if jwtData and cache_token == token:
-            user = User.objects.get(username=jwtData.get('username'))
-            request.META['user'] = user
-            if not user.last_login == None or user.is_superuser:
+        if token :
+            jwtData = JWTAuth.verifyToken(token)
+            cache = Cache.getCacheInstance()
+            cache_token = None
+            try:
+                cache_token = cache.hget(jwtData.get('username'), 'auth')
+            except Exception:
+                pass
+            if cache_token:
+                cache_token = cache_token.decode('utf-8')
+            if jwtData and cache_token == token:
+                user = User.objects.get(username=jwtData.get('username'))
+                request.META['user'] = user
                 return self.get_response(request, *args, **kwargs)
-            return JsonResponse({'response': 'You need to change password to access this resource'},
-                                status=status.HTTP_403_FORBIDDEN)
         return JsonResponse({'response': 'You have to login to access this resource',
                              'path': f"{reverse('login')}?next={request.path}"}, status=status.HTTP_401_UNAUTHORIZED)
-
-
-class TokenAuthenticationOnFirstAccess(object):
-    def __init__(self, get_response):
-        self.get_response = get_response
-
-    def __call__(self, request, token):
-        jwtData = JWTAuth.verifyToken(request.headers.get('Authorization'))
-        if jwtData:
-            user = User.objects.get(username=jwtData.get('username'))
-            request.META['user'] = user
-            return self.get_response(request, token)
-        return JsonResponse({'response': 'You are not logged in!'}, status=status.HTTP_403_FORBIDDEN)
 
 
 class CantAccessAfterLogin(object):

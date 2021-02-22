0de63fe08bf3037c71f8a356995e80b1b77543f2
prayas2409 <prayas.hemraj.pagade@gmail.com>
2021-02-22 17:57:02 +0530

    [Prayas] Added: removed enum

--- Auth/permissions.py
+++ Auth/permissions.py
@@ -1,30 +1,24 @@
 from rest_framework import permissions
 import enum
 
-
-class Role(enum.Enum):
-
-    ADMIN = 'Admin'
-    MENTOR = 'Mentor'
-    STUDENT = 'Engineer'
-
+from Auth.models import Roles
 
 class isAdmin(permissions.BasePermission):
     message = {'response': 'You are not an admin. Access Denied!'}
 
     def has_permission(self, request, view):
-        return request.META['user'].role == Role.ADMIN.value or request.META['user'].is_superuser
+        return request.META['user'].role == Roles.objects.get(role='admin') or request.META['user'].is_superuser
 
 
 class isMentorOrAdmin(permissions.BasePermission):
     message = {'response': 'You are not an Admin or a Mentor. Access Denied!'}
 
     def has_permission(self, request, view):
-        return request.META['user'].role == Role.MENTOR.value or request.META['user'].role == Role.ADMIN.value
+        return request.META['user'].role == Roles.objects.get(role='mentor') or request.META['user'].role == Roles.objects.get(role='admin')
 
 
 class OnlyStudent(permissions.BasePermission):
     message = {'response': 'Only student can access'}
 
     def has_permission(self, request, view):
-        return request.META['user'].role == Role.STUDENT.value
+        return request.META['user'].role == Roles.objects.get(role='student')

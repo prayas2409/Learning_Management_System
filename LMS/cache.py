5af52c0bafabc6a72f4ada53aa371da98437aeb7
prayas2409 <prayas.hemraj.pagade@gmail.com>
2021-02-24 19:19:15 +0530

    [Prayas] Refactor: App needs password to connect with databse

--- LMS/cache.py
+++ LMS/cache.py
@@ -1,6 +1,6 @@
 import redis
 from decouple import config
-from LMS.settings import REDIS_HOST, REDIS_PORT
+from LMS.settings import REDIS_HOST, REDIS_PORT, REDIS_PASS
 
 class Cache: 
     obj = None
@@ -8,6 +8,6 @@ class Cache:
     @staticmethod
     def getCacheInstance():
         if Cache.obj is None:
-            Cache.obj = redis.Redis(host=REDIS_HOST, port=REDIS_PORT)
+            Cache.obj = redis.Redis(host=REDIS_HOST, port=REDIS_PORT,password=REDIS_PASS)
         return Cache.obj
 #  redis configuration

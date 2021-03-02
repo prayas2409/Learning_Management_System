77f581bb3d0781caf0c4720b0e1ad095b79b4eff
prayas2409 <prayas.hemraj.pagade@gmail.com>
2021-03-02 18:05:30 +0530

    [Prayas] Added: Expose headers for auth token

--- Auth/views.py
+++ Auth/views.py
@@ -129,6 +129,7 @@ class UserLoginView(GenericAPIView):
                 status=status.HTTP_200_OK)
             jwt_token = JWTAuth.getToken(username=username, password=password)
             response['Authorization'] = jwt_token
+            response["Access-Control-Allow-Headers"]="Authorization"
             # token is storing in redis cache
             cache = Cache.getCacheInstance()
             cache.hmset(username, {'auth': jwt_token})
